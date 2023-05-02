ecae816db73c8ba679180ac74c9e1eb32dc11da6
Hector 1120 <lizcanoagudelo@gmail.com>
2023-05-02 14:55:50 -0500

    My first App

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first aplication</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Identificador</label>
+    <input type="text" class="form-control" id="Identificador" aria-describedby="emailHelp" name="Identificador">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nombre</label>
+    <input type="text" class="form-control" id="Nombre" name="Nombre">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Detalle</label>
+    <input type="text" class="form-control" id="Detalle" name="Detalle">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</body>
+</html>
